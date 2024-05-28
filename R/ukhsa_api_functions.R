0242e22e5fdfb29fb12044e0d65436dc9c2bdbfe
Ollie Lloyd <o.lloyd@doctors.org.uk>
2024-05-28 01:01:05 +0100

    change to using jsonlite which returns data.frame without need to use purrr & tidyr

--- R/ukhsa_api_functions.R
+++ R/ukhsa_api_functions.R
@@ -1,33 +1,30 @@
-library(tibble)
-library(tidyr)
-library(purrr)
 library(httr2)
-
+library(jsonlite)
 
 #' Get Paginated Data
 #' 
 #' Generates API query URL, retrieves paginated data and parses results into JSON format. If an API parameter 
 #' is not provided then a list of possible parameters is returned.
 #' 
-#' @param theme      the largest overall topical subgroup of data for example \code{infectious_disease}.
+#' @param theme           the largest overall topical subgroup of data for example \code{infectious_disease}.
 #' 
-#' @param sub_theme  a topical subgroup associated with the parent theme. for example \code{respiratory}.
+#' @param sub_theme       a topical subgroup associated with the parent theme. for example \code{respiratory}.
 #'                   
-#' @param topic      categorical subgroup associated with the selected theme and sub_theme. For example a topic 
+#' @param topic           categorical subgroup associated with the selected theme and sub_theme. For example a topic 
 #' of \code{COVID-19} would only be available for a theme of \code{infectious_disease} and a sub_theme of 
 #' \code{respiratory}.
 #' 
-#' @param geography_type the overarching area type for the intended geography for example \code{Nation}.
+#' @param geography_type  the overarching area type for the intended geography for example \code{Nation}.
 #' 
-#' @param geography   the selected area under the \code{geography_type}. For example \code{England}.
+#' @param geography       the selected area under the \code{geography_type}. For example \code{England}.
 #' 
-#' @param metric      the type of data being selected. For example \code{COVID-19_testing_PCRcountByDay}.
+#' @param metric          the type of data being selected. For example \code{COVID-19_testing_PCRcountByDay}.
 #' 
-#' @param page_number define which page of data to retrieve
+#' @param page_number     define which page of data to retrieve
 #' 
-#' @param page_size   define number of results returned per page. Maximum supported size is 365.
+#' @param page_size       define number of results returned per page. Maximum supported size is 365.
 #' 
-#' @return            Data for the given query or a list of parameter options.
+#' @return                Data for the given query or a list of parameter options.
 
 get_paginated_data <- function(theme = NULL, 
                                sub_theme = NULL, 
@@ -38,26 +35,26 @@ get_paginated_data <- function(theme = NULL,
                                page_number = 1,
                                page_size = 365) {
   
-  # create API request URL 
+  # create API endpoint URL 
   if (is.null(theme)) {
     url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes")
   } else {
     if (is.null(sub_theme)) {
-      url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", theme, "sub_themes", sep = "/")
+      url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", gsub(" ", "%20", theme), "sub_themes", sep = "/")
     } else {
       if (is.null(topic)) {
-        url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", theme, "sub_themes", sub_theme, "topics", sep = "/")
+        url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", gsub(" ", "%20", theme), "sub_themes", gsub(" ", "%20", sub_theme), "topics", sep = "/")
       } else {
         if (is.null(geography_type)) {
-          url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", theme, "sub_themes", sub_theme, "topics", topic, "geography_types", sep = "/")
+          url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", gsub(" ", "%20", theme), "sub_themes", gsub(" ", "%20", sub_theme), "topics", gsub(" ", "%20", topic), "geography_types", sep = "/")
         } else {
           if (is.null(geography)) {
-            url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", theme, "sub_themes", sub_theme, "topics", topic, "geography_types", geography_type, "geographies", sep = "/")
+            url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", gsub(" ", "%20", theme), "sub_themes", gsub(" ", "%20", sub_theme), "topics", gsub(" ", "%20", topic), "geography_types", gsub(" ", "%20", geography_type), "geographies", sep = "/")
           } else {
             if (is.null(metric)) {
-              url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", theme, "sub_themes", sub_theme, "topics", topic, "geography_types", geography_type, "geographies", geography, "metrics", sep = "/")
+              url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", gsub(" ", "%20", theme), "sub_themes", gsub(" ", "%20", sub_theme), "topics", gsub(" ", "%20", topic), "geography_types", gsub(" ", "%20", geography_type), "geographies", gsub(" ", "%20", geography), "metrics", sep = "/")
             } else {
-              url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", theme, "sub_themes", sub_theme, "topics", topic, "geography_types", geography_type, "geographies", geography, "metrics", metric, sep = "/")
+              url <- paste("https://api.ukhsa-dashboard.data.gov.uk/themes", gsub(" ", "%20", theme), "sub_themes", gsub(" ", "%20", sub_theme), "topics", gsub(" ", "%20", topic), "geography_types", gsub(" ", "%20", geography_type), "geographies", gsub(" ", "%20", geography), "metrics", gsub(" ", "%20", metric), sep = "/")
             }
           }
         }
@@ -65,66 +62,92 @@ get_paginated_data <- function(theme = NULL,
     }
   }
   
-  url <- stringr::str_replace_all(url, " ", "%20")
-  
   # get response for url for use in error control
   response <- httr2::request(url) |>
-    httr2::req_timeout(10) |> 
+    httr2::req_timeout(10) |>
     httr2::req_perform()
   
-  # Handle errors
-  if(is.null(theme)|is.null(sub_theme)|is.null(topic)|is.null(geography_type)|is.null(geography)|is.null(metric)){
-    # if parameters not filled out completely return possible parameters for that level
-    result <- httr2::request(url) |>
-      httr2::req_perform() |>
-      httr2::resp_body_string() |>
-      stringr::str_extract_all('(?<=\\"name\\":\\")[^\\"]*') |>
-      unlist()
+  # get parameters when query not fully completed
+  parameters <- httr2::request(url) |>
+    httr2::req_perform() |>
+    httr2::resp_body_string() |>
+    jsonlite::fromJSON() |> 
+    _[["name"]]
+  
+  # Handle errors; return parameters if not completed fully; return result if query fully entered
+  if (response$status_code >= 400) {
+    stop(httr2::resp_status(response))
   } else {
-    if (response$status_code >= 400) {
-      stop(httr2::resp_status(response))
+    if (response$status_code == 204) {
+      response <- NULL
     } else {
-      if (response$status_code == 204) {
-        response <- NULL
+      if(is.null(theme)) {
+        writeLines("\nTheme:\n")
+        return(parameters)
       } else {
-        # add page size/number query & return data in JSON format
-        result <- httr2::request(url) |> 
-          httr2::req_url_query(page_size = page_size, page = page_number) |> 
-          httr2::req_perform() |> 
-          httr2::resp_body_json()
+        if (is.null(sub_theme)) {
+          writeLines("\nSub Theme:\n")
+          return(parameters)
+        } else {
+          if (is.null(topic)) {
+            writeLines("\nTopic:\n")
+            return(parameters)
+          } else {
+            if (is.null(geography_type)) {
+              writeLines("\nGeography Type:\n")
+              return(parameters)
+            } else {
+              if (is.null(geography)) {
+                writeLines("\nGeography:\n")
+                return(parameters)
+              } else {
+                if (is.null(metric)) {
+                  writeLines("\nMetric:\n")
+                  return(parameters)
+                } else {
+                  # add page size/number query & return paginated results
+                  result <- httr2::request(url) |> 
+                    httr2::req_url_query(page_size = page_size, page = page_number) |> 
+                    httr2::req_perform() |> 
+                    httr2::resp_body_string() |> 
+                    jsonlite::fromJSON()
+                  
+                  return(result)
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
-  
-  return(result)
-  
 }
 
 
 #' Get Data
 #' 
-#' Interatively runs query to download all pages, combine the results and return a tibble.
+#' Iteratively runs query to download all results and combine them into a `data.frame`.
 #' 
 #' For further information on the UKHSA dashboard API please visit 
 #' the \href{https://ukhsa-dashboard.data.gov.uk/access-our-data}{API documentation}.
 #' 
-#' @param theme      the largest overall topical subgroup of data, for example \code{infectious_disease}.
+#' @param theme           the largest overall topical subgroup of data, for example \code{infectious_disease}.
 #' 
-#' @param sub_theme  a topical subgroup associated with the parent theme, for example \code{respiratory}.
+#' @param sub_theme       a topical subgroup associated with the parent theme, for example \code{respiratory}.
 #'                   
-#' @param topic      categorical subgroup associated with the selected theme and sub_theme. For example a topic 
+#' @param topic           categorical subgroup associated with the selected theme and sub_theme. For example a topic 
 #' of \code{COVID-19} would only be available for a theme of \code{infectious_disease} and a sub_theme of 
 #' \code{respiratory}.
 #' 
-#' @param geography_type the overarching area type for the intended geography, for example \code{Nation}.
+#' @param geography_type  the overarching area type for the intended geography, for example \code{Nation}.
 #' 
-#' @param geography  the selected area under the \code{geography_type}, for example \code{England}.
+#' @param geography       the selected area under the \code{geography_type}, for example \code{England}.
 #' 
-#' @param metric     the type of data being selected, for example \code{COVID-19_testing_PCRcountByDay}.
+#' @param metric          the type of data being selected, for example \code{COVID-19_testing_PCRcountByDay}.
 #' 
-#' @return           List of parameter options.
+#' @return                List of parameter options.
 #'                   
-#' @return           Tibble of data for the given \code{parameters}.
+#' @return                data.frame for the given query.
 #' 
 #' @export
 #' 
@@ -138,48 +161,45 @@ get_paginated_data <- function(theme = NULL,
 #'           metric = "COVID-19_cases_casesByDay"
 #'           )
 
-get_data <- function(theme = NULL, 
-                     sub_theme = NULL, 
-                     topic = NULL, 
-                     geography_type = NULL, 
-                     geography = NULL, 
+get_data <- function(theme = NULL,
+                     sub_theme = NULL,
+                     topic = NULL,
+                     geography_type = NULL,
+                     geography = NULL,
                      metric = NULL) {
   
   results <- list()
   current_page <- 1
   
   repeat {
-    # print(current_page)  # for testing purposes
-    paginated_results <- get_paginated_data(theme = theme, 
-                                            sub_theme = sub_theme, 
-                                            topic = topic, 
-                                            geography_type = geography_type, 
-                                            geography = geography, 
-                                            metric = metric,
-                                            page_number = current_page)
+
+    paginated_results <- get_paginated_data(
+      theme = theme,
+      sub_theme = sub_theme,
+      topic = topic,
+      geography_type = geography_type,
+      geography = geography,
+      metric = metric,
+      page_number = current_page
+    )
     
-    if (is.null(paginated_results))
+    if (is.null(paginated_results)) {
       break
+    }
     
-    if (!("results" %in% names(paginated_results))) {
+    if (class(paginated_results) != "list") {
       return(paginated_results)
     }
-
-    results <- c(results, paginated_results[["results"]])
     
-    # print(paginated_results[["next"]]) # for testing purposes
+    results <- rbind(results, paginated_results$results)
     
-    if (is.null(paginated_results[["next"]]))
+    if (is.null(paginated_results$`next`)) {
       break
+    }
     
     current_page <- current_page + 1
-    
+
   }
-  
-  results <- results |>
-    purrr::transpose() |>
-    tibble::as_tibble() |>
-    tidyr::unnest(cols = tidyr::everything())
   
   return(results)
   
